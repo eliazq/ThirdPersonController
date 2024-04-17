93598ecd92f578d33ad6c6d93f74a58ad0d27ddd
Elias Heijari <69460338+elkkuzq@users.noreply.github.com>
2024-04-17 13:04:05 +0300

    Third Person Movement

---
+++ Assets/Scripts/ThirdPersonAnimationController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonAnimationController : MonoBehaviour
+{
+    [SerializeField] ThirdPersonController thirdPersonController;
+    [SerializeField] Animator animator;
+    [SerializeField] private float blendTreeSmoothTime = 0.1f;
+
+    private const string isWalking = "IsWalking";
+    private const string isRunning = "IsRunning";
+    private const string jumpUp = "JumpUp";
+    private const string jumpForward = "JumpForward";
+    private const string isFalling = "Falling";
+    private const string isLanding = "Landing";
+
+    private void Start()
+    {
+        if (animator == null) { }
+            animator = GetComponent<Animator>();
+
+        if (thirdPersonController == null)
+            thirdPersonController = GetComponentInParent<ThirdPersonController>();
+
+        thirdPersonController.OnJump += thirdPersonController_OnJump;
+    }
+
+    private void thirdPersonController_OnJump(object sender, ThirdPersonController.OnJumpEventArgs e)
+    {
+        if (e.jumpUp) animator.SetTrigger(jumpUp);
+        else if (e.jumpForward) animator.SetTrigger(jumpForward);
+    }
+
+    private void Update()
+    {
+        animator.SetBool(isWalking, thirdPersonController.IsWalking);
+        animator.SetBool(isRunning, thirdPersonController.IsRunning);
+        animator.SetBool(isFalling, thirdPersonController.IsFalling);
+        animator.SetBool(isLanding, thirdPersonController.IsLanding);
+    }
+}
--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,22 +7,87 @@ using UnityEngine;
 public class ThirdPersonController : MonoBehaviour
 {
 
-    [SerializeField] private float movementSpeed = 2.5f;
+    public event EventHandler<OnJumpEventArgs> OnJump;
+    public class OnJumpEventArgs : EventArgs
+    {
+        public bool jumpForward;
+        public bool jumpUp;
+    }
+
+    [Header("References")]
+    [SerializeField] private Camera playerCamera;
     [SerializeField] private CharacterController characterController;
-    [SerializeField] private Transform playerCamera;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    [Header("Settings Camera")]
+    [SerializeField] private float runningFovAmount = 5f;
+    [SerializeField] private float timeToRunningFov = 18f;
+    float walkingFov;
+    float runningFov;
+
+    [Header("Settings Movement")]
+    [SerializeField] private float walkingSpeed = 2.5f;
+    [SerializeField] private float runningSpeed = 4f;
+    [SerializeField] private float jumpHeight = 1.6f;
+    [SerializeField] private float groundCheckRadius = 0.4f;
     [SerializeField, Range(0f, 0.3f)] float rotationSmoothTime = 0.1f;
     private float rotationSmoothVelocity;
+    private float jumpCooldown = 1f;
+    public Vector3 MoveDirection { get; private set; }
+    private float movementSpeed = 3f;
+    Vector3 lastMoveDirection;
+    float lastMovementSpeed;
+    float jumpTimer;
+    bool isWalking;
+    public bool IsWalking { get { return isWalking && IsRunning == false; } }
+    public bool IsRunning
+    {
+        get
+        {
+            return isWalking && movementSpeed > walkingSpeed;
+        }
+    }
+    public bool IsFalling
+    {
+        get
+        {
+            return IsGrounded == false && velocity.y < -3f;
+        }
+    }
+    public bool IsLanding { get; private set; }
+
+    [Header("Settings Gravity")]
+    [SerializeField] private float gravity = -9.81f;
+    public float Gravity { get { return gravity; } }
+    public bool IsGrounded { get; private set; }
+    Vector3 velocity;
 
     private void Start()
     {
+        // If camera not set, try get it from tag MainCamera
+        if (playerCamera == null) playerCamera = Camera.main;
+
+        // If characterController not set, try to get it from this GameObject
         if (characterController == null) characterController = GetComponent<CharacterController>();
-        if (playerCamera == null) playerCamera = Camera.main.transform;
+
+        // If groundCheck not set, try find it in child
+        if (groundCheck == null)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                if (transform.GetChild(i).name.ToLower() == "groundcheck") groundCheck = transform.GetChild(i);
+            }
+        }
+        walkingFov = playerCamera.fieldOfView;
+        runningFov = walkingFov + runningFovAmount;
     }
 
     private void Update()
     {
-        Movement();
+        AddGravity();
 
+        Movement();
     }
     private void Movement()
     {
@@ -31,15 +97,99 @@ public class ThirdPersonController : MonoBehaviour
         
         // Move Direction
         Vector3 moveDirection = new Vector3(xInput, 0, zInput).normalized;
-        if (moveDirection.magnitude < 0.1f) return; 
-        
+
+        MoveDirection = moveDirection;
         // Rotate Towards MoveDirection
-        float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+        
+        float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + playerCamera.transform.eulerAngles.y;
         float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSmoothVelocity, rotationSmoothTime);
-        transform.rotation = Quaternion.Euler(transform.rotation.x, angle, transform.rotation.z);
+        if (moveDirection.magnitude > 0.1f && IsGrounded)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, angle, transform.rotation.z);
+        }
+
+        // Running
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = runningSpeed;
+        }
+        else
+        {
+            movementSpeed = walkingSpeed;
+        }
 
         // Move
         Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-        characterController.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
+
+        if (moveDirection.magnitude > 0.1f && IsGrounded)
+            characterController.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
+
+        if (IsGrounded)
+        {
+            if (moveDirection.magnitude > 0.1f) isWalking = true;
+            else isWalking = false;
+            IsLanding = false;
+            lastMovementSpeed = movementSpeed;
+            lastMoveDirection = moveDir;
+        }
+        else
+        {
+            isWalking = false;
+            characterController.Move(lastMoveDirection * lastMovementSpeed * Time.deltaTime);
+        }
+
+        jumpTimer += Time.deltaTime;
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded && jumpTimer >= jumpCooldown)
+        {
+            jumpTimer = 0;
+            bool jumpWithMovement = lastMoveDirection != Vector3.zero;
+            OnJump?.Invoke(this, new OnJumpEventArgs
+            {
+                jumpForward = jumpWithMovement,
+                jumpUp = !jumpWithMovement
+            });
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        UpdateCameraFov();
+    }
+
+    private void AddGravity()
+    {
+        IsGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (IsGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+            CheckLanding();
+        }
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    private void UpdateCameraFov()
+    {
+        runningFov = walkingFov + runningFovAmount;
+        // if running or falling
+        if (IsRunning || velocity.y < 0.1f && !IsGrounded)
+        {
+            playerCamera.fieldOfView = Mathf.MoveTowards(playerCamera.fieldOfView, runningFov, timeToRunningFov * Time.deltaTime);
+        }
+        else
+        {
+            playerCamera.fieldOfView = Mathf.MoveTowards(playerCamera.fieldOfView, walkingFov, timeToRunningFov * Time.deltaTime);
+        }
+    }
+
+    private void CheckLanding()
+    {
+        if (Physics.SphereCast(transform.position, characterController.radius, -transform.up, out RaycastHit hit, characterController.height * 2))
+        {
+            IsLanding = true;
+        }
     }
 }
